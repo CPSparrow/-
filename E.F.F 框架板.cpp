70484d759576f9a434f8b71bae176de20aa4ac26
Old Wan <36687023+CPSparrow@users.noreply.github.com>
2019-03-10 10:29:07 +0800

    Add files via upload
    上传框架版

---
+++ E.F.F 框架板.cpp
@@ -0,0 +1,43 @@
+//write head here
+short result = 0;
+bool isW = 0;
+short info[8];
+void joke(){
+	//do something
+}
+
+void setGame(){
+	//do something
+}
+
+void play(){
+	while(info[4] != 0){
+		//do something
+	}
+}
+
+int  inPut(){
+	//do something
+}
+
+void goodEnd(){
+	//do something
+}
+int main() {
+	result = inPut();
+	if(result == 1) {
+		joke();
+	}else if(result == -1){
+		isW = 0;
+	}
+	setGame();
+	if(isW == 1){
+		play();
+	}
+	if(isW == 0){
+		//poor end because input is wrong
+	}else {
+		goodEnd;
+	}
+	return 0;
+}
